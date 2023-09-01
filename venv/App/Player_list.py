da60757b436ec94d569af99569a1d63dc19cd26b
PlayVoidz510 <crazylol457@gmail.com>
2023-09-01 09:55:37 +0800

    Ability to add items to the front of the linked list and sort the last item to the tail of the list, added unit tests for is the list empty and for pushing items.

--- venv/App/Player_list.py
+++ venv/App/Player_list.py
@@ -1,3 +1,22 @@
+from App.Player_node import PlayerNode
 class PlayerList:
     def __init__(self):
         self.head = None # Initally there are no elements in the list
+        self.tail = None
+
+    def push_front(self, new):  # Adding an element before the first element
+        new_node = PlayerNode(new)  # new node
+        new_node.next = self.head
+
+        if self.head != None:  # Checks whether list is empty or not
+            self.head.prev = new_node  # old head's previous value to new player node
+            self.head = new_node  # new node becomes the new head
+            new_node.prev = None
+
+        else:  # If list is blank, make new node head and tail
+            self.head = new_node
+            self.tail = new_node
+            new_node.prev = None
+
+    def is_empty(self):
+        return self.head is None and self.tail is None
---
+++ venv/Test/Player_list_test.py
@@ -0,0 +1,56 @@
+# test_player_list.py
+import unittest
+from App.Player_list import PlayerList
+from App.Player_node import PlayerNode  # Import the PlayerNode class from player_node.py
+from App.Player import Player  # Import the Player class from the appropriate location
+
+class TestPlayerList(unittest.TestCase):
+
+    def test_push_front(self):
+        player_list = PlayerList()
+        player1 = Player(1, name='Player 1')
+        player2 = Player(2, name='Player 2')
+
+        player_list.push_front(player1)
+        player_list.push_front(player2)
+
+
+        # Pushing player 1 and then 2 should make player 1 the end time and player 2
+        # the new top item
+        self.assertEqual(player_list.head.player, player2)
+        self.assertEqual(player_list.tail.player, player1)
+
+
+    # Pushing from an empty list should have the head and tail as the same item
+    # as there is only one player anyway
+    def test_push_front_empty_list(self):
+        player_list = PlayerList()
+        player = Player(1, name='Player')
+
+        player_list.push_front(player)
+
+        self.assertEqual(player_list.head.player, player)
+        self.assertEqual(player_list.tail.player, player)
+
+
+    # test with multiple players for head and tail (front/back)
+    def test_push_front_multiple_players(self):
+        player_list = PlayerList()
+        player1 = Player(1, name='Player 1')
+        player2 = Player(2, name='Player 2')
+        player3 = Player(3, name='Player 3')
+
+        player_list.push_front(player1)
+        player_list.push_front(player2)
+        player_list.push_front(player3)
+
+        self.assertEqual(player_list.head.player, player3)
+        self.assertEqual(player_list.tail.player, player1)
+
+    def test_empty_list(self):
+        player_list = PlayerList()
+
+        self.assertTrue(player_list.is_empty())
+
+if __name__ == '__main__':
+    unittest.main()
